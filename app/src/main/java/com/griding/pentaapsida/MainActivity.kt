fdca5bb62186f50c6a5fa4b8d4c85526fdb6563f
rizqifakhri1 <rizqifakhri@gmail.com>
2022-06-23 07:27:58 +0700

    Initial commit

---
+++ app/src/main/java/com/griding/pentaapsida/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.griding.pentaapsida
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //Turn Off dark mode
+        setFullScreen(window)
+        lightStatusBar(window)
+    }
+}
